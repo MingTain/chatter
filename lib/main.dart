a799cb6131ea452cfc205924cc7853e25b9320a4
Kexuan Yang <yangkx@seagroup.com>
2019-04-29 16:29:26 +0800

    Add Google login
    Signed-off-by: Kexuan Yang <yangkx@seagroup.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
 import 'contact.dart';
-import 'constants.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(MyApp());
@@ -63,6 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   String name = '';
+  String avatarUrl = '';
   bool needRefresh = false;
 
   @override
@@ -72,10 +72,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   _initLogin() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int userId = prefs.getInt(PREF_KEY_USER_ID);
-    print("user id: $userId");
-    if (userId == null || userId == 0) {
+    var currentUser = await _auth.currentUser();
+    if (currentUser == null) {
       final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
       final GoogleSignInAuthentication googleAuth = await googleUser
           .authentication;
@@ -83,39 +81,58 @@ class _MyHomePageState extends State<MyHomePage> {
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
-      print("google id token: ${googleAuth.idToken}");
-
-      final FirebaseUser user = await _auth.signInWithCredential(credential);
-      print("signed in ${user.displayName}, id: ${googleUser.id}");
-      final idToken = await user.getIdToken();
-      print("id token: $idToken");
+      currentUser = await _auth.signInWithCredential(credential);
+      final idToken = await currentUser.getIdToken();
+      print("signed in success, id: ${currentUser.uid}, token: $idToken, name: ${currentUser.displayName}");
+    } else {
+      await currentUser.reload();
     }
+    if (currentUser == null) {
+      return;
+    }
+    setState(() {
+      name = currentUser.displayName;
+      avatarUrl = currentUser.photoUrl;
+    });
+  }
+
+  _logout() async {
+    _googleSignIn.signOut();
+    Navigator.pop(context);
+    setState(() {
+      name = '';
+      avatarUrl = '';
+    });
+    // TODO show login tip
   }
 
   _refresh() async {
     // TODO load msg
-    needRefresh = false;
     setState(() {
-      name = 'Need refresh';
+      needRefresh = false;
     });
   }
 
   _startChat() async {
-      var contacts = <Contact>[
-        Contact('Yang'),
-        Contact('Zhang'),
-        Contact('Huang'),
-        Contact('Liu'),
-        Contact('Zhao'),
-        Contact('Tom')
-      ]..sort();
-      final result = await Navigator.push<Contact>(context, MaterialPageRoute(
-          builder: (context) => ContactPage(contacts: contacts)));
-      setState(() {
-        if (result != null) {
-          name = result.name;
-        }
-      });
+    if (await _auth.currentUser() == null) {
+      _initLogin();
+      return;
+    }
+    var contacts = <Contact>[
+      Contact('Yang'),
+      Contact('Zhang'),
+      Contact('Huang'),
+      Contact('Liu'),
+      Contact('Zhao'),
+      Contact('Tom')
+    ]..sort();
+    final result = await Navigator.push<Contact>(context, MaterialPageRoute(
+        builder: (context) => ContactPage(contacts: contacts)));
+    setState(() {
+      if (result != null) {
+        name = result.name;
+      }
+    });
   }
 
   _getWelcomeText() {
@@ -123,6 +140,14 @@ class _MyHomePageState extends State<MyHomePage> {
       return Text('Welcome!');
     } else {
       return Text('Welcome $name!');
+    }
+  }
+
+  ImageProvider _getAvatar() {
+    if (avatarUrl.isEmpty) {
+      return MemoryImage(kTransparentImage);
+    } else {
+      return NetworkImage(avatarUrl);
     }
   }
 
@@ -145,6 +170,39 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: _getWelcomeText(),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              margin: EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColorDark,
+              ),
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: _getAvatar(),
+                    maxRadius: 30,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                        name,
+                        style: TextStyle(color: Colors.white, fontSize: 18)
+                    ),
+                  )
+
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text('Logout'),
+              onTap: _logout,
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _startChat,
